7e8ddee5115160ea405dbe25cfc55bd3e8a292ad
jsigee87 <johnsigmon@gmail.com>
2018-01-16 13:41:03 -0600

    downloaded source code

---
+++ lab1/src/SortTools.java
@@ -0,0 +1,26 @@
+// SortTools.java 
+/*
+ * EE422C Project 1 submission by
+ * Replace <...> with your actual data.
+ * <Student Name>
+ * <Student EID>
+ * <5-digit Unique No.>
+ * Spring 2017
+ * Slip days used: 
+ */
+
+package assignment1;
+public class SortTools {
+	/**
+	  * This method tests to see if the given array is sorted.
+	  * @param x is the array
+	  * @param n is the size of the input to be checked
+	  * @return true if array is sorted
+	  */
+	public static boolean isSorted(int[] x, int n) {
+		// stub only, you write this!
+		return false;
+	}
+	
+	// more functions required
+}
