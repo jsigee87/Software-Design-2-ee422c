2de38333125b6d4318d015cfea8e2b65f3bb4d5f
jsigee87 <johnsigmon@gmail.com>
2018-01-17 16:03:30 -0600

    wrote first few methods, no debugging done

--- lab1/src/SortTools.java
+++ lab1/src/SortTools.java
@@ -1,12 +1,11 @@
 // SortTools.java 
 /*
  * EE422C Project 1 submission by
- * Replace <...> with your actual data.
- * <Student Name>
- * <Student EID>
- * <5-digit Unique No.>
- * Spring 2017
- * Slip days used: 
+ * Written by: John Sigmon
+ * EID: js85773
+ * 15505
+ * Spring 2018
+ * Slip days used: 0 
  */
 
 package assignment1;
@@ -18,9 +17,82 @@ public class SortTools {
 	  * @return true if array is sorted
 	  */
 	public static boolean isSorted(int[] x, int n) {
-		// stub only, you write this!
-		return false;
+		/* Given Assumptions:
+         * - n <= len(x)
+         * - n >= 0
+         * - x is not null
+         */
+
+        // Corner Cases (given by assignment instructions)
+        if (x.len == 0){
+            return false;
+        }
+        if (n == 0){
+            return false;
+        }
+        if (n == 1){
+            return true;
+        }
+
+        // Grab first array element and then compare
+        // succeeding elements
+        int prev = x[0];
+        
+        for (int i = 1; i < n - 1; i ++){
+            if (x[i] < prev){
+                return false;
+            }
+            else {
+                prev = x[i];
+            }
+        }
+        
+        return true;
 	}
-	
-	// more functions required
+
+    public static int find(int[] x, int n, int target) {
+        /* Given Assumptions:
+         * - n <= len(x)
+         * - n > 0
+         * - x is not null
+         */
+
+        if (x.len == 0){
+           return false;
+        }
+        if (n == 0){
+           return false;
+        }
+        if (n == 1){
+            if (v == x[0]){
+                return 0;
+            }
+        }
+
+        // Else we do binary search
+        int start = 0;
+        int end = n;
+        while (start <= end){
+            if (x[n/2] > target){
+                end = n/2;
+            }
+            else if(x[n/2] < target){
+                start = n/2;
+            }
+            else if(x[n/2] == target){
+                return n/2;
+            }
+        }
+        
+        // If we end binary search and reach here
+        // then our target is not in x.
+        return -1;
+
+    }
+
+    public static int insertGeneral(int[] x, int n, int target) {
+
+
+
+    }    
 }
